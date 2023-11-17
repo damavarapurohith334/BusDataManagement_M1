a25eaa8cd70c28696ffd54966eb408d72da5d877
Damavarupu Rohith <damavarapurohith334>
2023-11-17 16:33:40 +0530

    first commit

--- src/main/java/com/busData/utils/ValidationConstants.java
+++ src/main/java/com/busData/utils/ValidationConstants.java
@@ -4,12 +4,7 @@ public enum ValidationConstants {
 	
 	BUS_NOT_FOUND("bus.not.found"), BUS_ALREADY_EXIST("bus.already.exist"),
 
-	DIRECTOR_FIRST_NAME_NOT_EMPTY("director.firstName.notEmpty"),
-	DIRECTOR_LAST_NAME_NOT_EMPTY("director.lastName.notEmpty"),
-	DIRECTOR_CONTACT_NUMBER_PATTERN("director.contactNumber.pattern"), 
-	DIRECTOR_EMAIL_PATTERN("director.email.pattern"),
-
-	ROUTE_NOT_FOUND("Route.not.Found"),
+	
 	SOMETHING_WENT_WRONG("somthing.went.wrong");
 
 	private final String messageKey;
